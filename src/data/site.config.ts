c66dd12ec1150ec64ee23572f9f57b95127858dd
lummoura <lummoura@gmail.com>
2024-10-23 20:54:36 +0200

    Update site.config.ts

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,9 +9,9 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'Luciana Monteiro de Moura', // Site author
+	title: 'Impatient Ms Science', // Site title.
+	description: "Luciana's Science Blog", // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
